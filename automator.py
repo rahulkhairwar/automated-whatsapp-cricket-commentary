d10da95e4b7479ab3992cf540b26887d32d9aa3c
Rahul Khairwar <rahulkhairwar@gmail.com>
2018-12-09 03:50:47 +0530

    Completed fetch teams/scores/commentaries

--- automator.py
+++ automator.py
@@ -2,12 +2,29 @@ import urllib.request
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+class Match:
+    def __init__(self, first_team, second_team, first_team_score, second_team_score):
+        self.first_team = first_team
+        self.second_team = second_team
+        self.first_team_score = first_team_score
+        self.second_team_score = second_team_score
+        self.commentary = []
+
+    def __repr__(self):
+        return str(self.__dict__)
+
 class Comment:
     def __init__(self, over, description):
+        self.over = over
+        self.description = description
         self.paragraphs = []
 
     def add_paragraph(self, paragraph):
         self.paragraphs.append(paragraph)
+
+    def __repr__(self):
+        # return str(self.__dict__)
+        return "over : {}, desc : {}, paras : {}".format(self.over, self.description, self.paragraphs)
 
 def get_team_name_and_score(soup, top_list_item_class_name):
     TEAM_NAME_CLASS_NAME = "cscore_team icon-font-after"
@@ -27,19 +44,17 @@ def get_commentary(soup):
     for commentary_item in root_div_tags_children:
         over = commentary_item.find("div", {"class":"time-stamp"})
         description = commentary_item.find("div", {"class":"description"})
-        # print("over : {}, desc : {}\n".format(over, description))
 
         if (over is None):
-            over = "-"
+            over = ""
         else:
             over = over.text
 
         if (description is None):
-            description = "-"
+            description = ""
         else:
             description = description.text
 
-        # if (over is not None and description is not None):
         comment = Comment(over, description)
         paragraphs = commentary_item.findAll("p", {"class":"comment"})
 
@@ -47,11 +62,21 @@ def get_commentary(soup):
             for p in paragraphs:
                 comment.add_paragraph(p.text)
 
-        commentary.append(comment)
+        if (len(over) != 0 or len(description) != 0 or len(comment.paragraphs) != 0):
+            # print("len(over) : {}, len(desc) : {}, paras.len : {}", len(over), len(description), len(comment.paragraphs))
+            commentary.append(comment)
+
+    commentary.reverse()
 
     return commentary
 
-def get_latest_commentary_from_espn(last_timestamp):
+"""Function to parse ESPN's Cricinfo website, and fetch match details using other functions.
+
+Can use the last_timestamp variable to get comments made only after the timestamp of the last comment fetched previously,
+but there are no timestamp fields in the HTML, and anyway the site loads just a few comments, while more load on the go
+by scrolling, so there's no desperate need to implement this for now.
+"""
+def get_match_info_from_espn(last_timestamp):
     MATCH_URL = "http://www.espncricinfo.com/series/18693/commentary/1144993/australia-vs-india-1st-test-india-in-aus-2018-19"
     FIRST_TEAM_TOP_LIST_ITEM_CLASS_NAME = "cscore_item cscore_item--home"
     SECOND_TEAM_TOP_LIST_ITEM_CLASS_NAME = "cscore_item cscore_item--away"
@@ -62,18 +87,20 @@ def get_latest_commentary_from_espn(last_timestamp):
     page_content = response.read().decode("utf-8")
     soup = BeautifulSoup(page_content, "html.parser")
 
-    first_team_name, first_team_mini_score = get_team_name_and_score(soup, FIRST_TEAM_TOP_LIST_ITEM_CLASS_NAME)
-    print("{} - {}".format(first_team_name, first_team_mini_score))
+    first_team_name, first_team_score = get_team_name_and_score(soup, FIRST_TEAM_TOP_LIST_ITEM_CLASS_NAME)
+    print("{} - {}".format(first_team_name, first_team_score))
 
-    second_team_name, second_team_mini_score = get_team_name_and_score(soup, SECOND_TEAM_TOP_LIST_ITEM_CLASS_NAME)
-    print("{} - {}".format(second_team_name, second_team_mini_score))
+    second_team_name, second_team_score = get_team_name_and_score(soup, SECOND_TEAM_TOP_LIST_ITEM_CLASS_NAME)
+    print("{} - {}".format(second_team_name, second_team_score))
 
+    match = Match(first_team_name, second_team_name, first_team_score, second_team_score)
     commentary = get_commentary(soup)
+    match.commentary = commentary
 
-    for comment in commentary:
-        print("Commentary : {}\n".format(comment))
+    # for comment in commentary:
+    #     print("Commentary => {}\n".format(comment))
 
-    return first_team_mini_score
+    return match
 
 def get_latest_commentary(last_timestamp):
     SCORE_CLASS_NAME = "cb-min-bat-rw"
@@ -89,15 +116,15 @@ def get_latest_commentary(last_timestamp):
 
     for data in soup.findAll("div", {"class":"{}".format(SCORE_CLASS_NAME)}):
         for item in data.findAll("span", {"class":"{}".format(MINI_SCORE_SPAN_CLASS_NAME)}):
-            first_team_mini_score = item.text
+            first_team_score = item.text
 
     for data in soup.findAll("div", {"ng-init":"comm = match.commentary"}):
         print(data)
         break
 
-    return first_team_mini_score
+    return first_team_score
 
-def send_messages():
+def send_messages_on_whatsapp():
     URL = "https://web.whatsapp.com"
     MESSAGE_BOX_CLASS_NAME = "_1Plpp"
     SEND_BUTTON_CLASS_NAME = "_35EW6"
@@ -113,14 +140,16 @@ def send_messages():
     count = 50
 
     for i in range(count):
+
+        # match_details = get_match_details_string()
         message_box.send_keys("test {}".format(i + 1))
         send_button = driver.find_element_by_class_name(SEND_BUTTON_CLASS_NAME)
         send_button.click()
 
 if __name__ == "__main__":
-    # send_messages()
+    # send_messages_on_whatsapp()
     # latest_commentary = get_latest_commentary(None)
     # print(latest_commentary)
 
-    espn_commentary = get_latest_commentary_from_espn(None)
+    espn_commentary = get_match_info_from_espn(None)
     print(espn_commentary)
