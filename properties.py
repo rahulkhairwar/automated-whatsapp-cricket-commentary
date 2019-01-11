8e1e10fc3904aef231dd8c932fcd3cb08cc8828f
RAHUL KHAIRWAR <rahulkhairwar@gmail.com>
2019-01-11 20:46:27 +0530

    Added requirements.txt for installing all modules at once

--- properties.py
+++ properties.py
@@ -1,4 +1,4 @@
-MATCH_URL = "http://www.espncricinfo.com/series/18693/commentary/1144995/australia-vs-india-3rd-test-india-in-aus-2018-19"
+MATCH_URL = "http://www.espncricinfo.com/series/18693/commentary/1144996/australia-vs-india-4th-test-india-in-aus-2018-19"
 
 # Match start timings according to where the script is being run.
 MATCH_START_HOURS = 5
@@ -7,5 +7,5 @@ MATCH_END_HOURS = 13
 MATCH_END_MINUTES = 0
 LOG_FILENAME = 'script_logs.log'
 
-# This tells whether the script is running in test mode(when we don't need to send all the data to a contact, just small amounts)
-IS_TEST_MODE = True
+# This tells whether the script is running in test mode or not. If yes, the actual data sent is less, for better debugging.
+IS_TEST_MODE = False
